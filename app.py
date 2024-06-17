f225ebe3faa18eb3e35c10ae2a3129a5956742f1
Tanya Chang <tanyachang@Tanyas-MacBook-Air.local>
2024-06-17 21:13:12 +0800

    completedeploy

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return 'Hello, World!'
 
-def hello():
-    return "Hello, World!"
+if __name__ == '__main__':
+    app.run()
+
